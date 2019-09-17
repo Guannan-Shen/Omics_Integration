e04033627359576f2428046853cb532e10088eec
Guannan-Shen <guannan.shen.ai@gmail.com>
2019-09-16 20:13:50 -0600

    density plot and correlation heatmap for clinical

--- Code/ref_plots.R
+++ Code/ref_plots.R
@@ -4,6 +4,7 @@
 options(stringsAsFactors = F)
 options(dplyr.width = Inf)
 
+library(reshape2)
 library(readxl)
 library(tidyverse)
 library(magrittr)
@@ -192,4 +193,75 @@ elbow_RA <- function(taxa_level, prev, RA_vector){
 
 contour_prev_RA <- function(taxa_level, prev_vector, RA_vector){
   
+}
+
+## heatmap of features based on correlation 
+
+cor_heatmap <- function(df, method, reorder, hclust_method, text_size){
+  print("Use pairwise.complete.obs, and methods from pearson, kendall, spearman")
+  cormat = cor(df, use = "pairwise.complete.obs", method = method)
+  ###### Reordered correlation data visualization #####
+  # using correlation as dist
+  reorder_cormat <- function(cormat){
+    # Use correlation between variables as distance
+    dd <- as.dist((1-cormat)/2)
+    # default settings of 
+    hc <- hclust(dd, method = hclust_method)
+    cormat <-cormat[hc$order, hc$order]
+  }
+  if(reorder){
+    cormat <- reorder_cormat(cormat)
+  }else{}
+  
+  # Get upper triangle of the correlation matrix
+  get_upper_tri <- function(cormat){
+    cormat[lower.tri(cormat)] = NA
+    return(cormat)
+  }
+  upper_tri = get_upper_tri(cormat)
+  
+  # reshape data 
+  melted_cormat = melt(upper_tri, na.rm = TRUE)
+  # Create a ggheatmap
+  ggheatmap <- ggplot(melted_cormat, aes(Var2, Var1, fill = value))+
+    geom_tile(color = "white")+
+    scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
+                         midpoint = 0, limit = c(-1,1), space = "Lab", 
+                         name= method) +
+    theme_minimal()+ # minimal theme
+    theme(axis.text.x = element_text(angle = 45, vjust = 1, 
+                                     size = 12, hjust = 1))+
+    coord_fixed() +
+    labs(x = "", y = "") + 
+    geom_text(aes(Var2, Var1, label = round(value,2) ), color = "black", size = text_size)  +
+    theme(
+      panel.grid.major = element_blank(),
+      panel.border = element_blank(),
+      panel.background = element_blank(),
+      axis.ticks = element_blank(),
+      legend.justification = c(1, 0),
+      legend.position = c(0.5, 0.8),
+      legend.direction = "horizontal")+
+    guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
+                                 title.position = "top", title.hjust = 0.5))
+  # Print the heatmap
+  print(ggheatmap)
+  return(cormat)
+}
+
+
+density_values_ind <- function(data, title){
+  print("A column called group and a column called values")
+  p = ggplot(data, aes(x = values, color = ind, fill = ind)) + 
+    # alpha controls the transparency 
+    geom_density(alpha = 0.6) +
+    # gray scale color
+    # grey scale plot
+    scale_colour_grey() +
+    # start = 0.2, end = 0.8
+    scale_fill_grey() +
+    theme_bw() +
+    theme(legend.position="bottom", legend.box = "horizontal") +
+    labs(caption = title) 
+  print(p)
 }
