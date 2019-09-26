d37bbda04b4dbe5ca6474ffc766e548d6ec4da75
Guannan-Shen <guannan.shen.ai@gmail.com>
2019-09-25 22:55:53 -0600

    bitr gene id transfer, barplot of pathways

--- Code/enrichment.R
+++ Code/enrichment.R
@@ -7,7 +7,24 @@ library(DOSE)
 library(magrittr)
 library(org.Hs.eg.db)
 library(enrichR)
+library(enrichplot)
 # Carlson M (2019). org.Hs.eg.db: Genome wide annotation for Human
+
+######## find most connected hubs #############
+# transform to dataframe
+
+top_hubs_bymean <- function(abar, modules_i, n){
+  # get modules[[i]] from abar
+  df = abar[modules_i, modules_i ] %>% as.matrix() %>% as.data.frame()
+  # edges strength by mean 
+  mean_edge = mean(apply(df, 2, mean))
+  edges_nodes =  apply(df > mean_edge, 1, sum)  %>%
+    as.data.frame() %>% tibble::rownames_to_column("Nodes")
+  colnames(edges_nodes)[2] = "n_strong_edges"
+  final = edges_nodes %>% dplyr::top_n(n) %>% plyr::arrange(plyr::desc(n_strong_edges) )
+  return(final)
+}
+
 
 # GO over-representaion test of Cellular Component
 clusterPro_GO <- function(symbols, ontol, fdrcut) {
@@ -26,6 +43,64 @@ clusterPro_GO <- function(symbols, ontol, fdrcut) {
   return(overGO)
 }
 
+eg = bitr(x, fromType="SYMBOL", toType="ENTREZID", OrgDb="org.Hs.eg.db")
+enrichKEGG(gene         = gene,
+           organism     = 'hsa',
+           keyType       = 'ncbi-geneid',
+           pvalueCutoff = 0.05)
+
+######## summary of clusterprofiler ########
+summary_clusterPro <- function(abar, modules, p1, ontol, fdrcut){
+  print(dim(abar)) # similarity matrix
+  n_networks = length(modules)
+  features = colnames(abar)
+  
+  for ( i in 1:n_networks){
+    print("Total nodes:")
+    print(length(modules[[i]]))
+    top_hubs_bymean(abar, modules[[i]], n = 20) %>% kable %>% print()
+  }
+  
+  # genelists 
+  
+  for ( i in 1:n_networks){
+    # gene index 
+    index = modules[[i]] [modules[[i]] <= p1]
+    genes = features[index]
+    print(i)
+
+    print("GO Cellular Component")
+    clusterPro_GO( genes, ontol, fdrcut) %>% kable %>% print()
+    
+    # print("KEGG")
+    # kegg_enrichr( genes) %>% kable %>% print()
+  }
+  
+}
+
+# edo <- enrichDGN(de)
+# library(enrichplot)
+# barplot(edo, showCategory=20)
+p1 = ncol(filtered_rlog[, filtered_outlier])
+index = modules[[5]] [modules[[5]] <= p1]
+features = colnames(abar)
+genes = features[index]
+cc_test <- clusterPro_GO( genes, "CC", 0.2)
+barplot(cc_test, showCategory = length(cc_test$qvalue) )
+
+cc_test$qvalue
+
+eg = bitr(genes, fromType="SYMBOL", toType="ENTREZID", OrgDb="org.Hs.eg.db")
+
+gg_test <- enrichKEGG(gene         = eg$ENTREZID,
+           organism     = 'hsa',
+           keyType       = 'ncbi-geneid',
+           pAdjustMethod = "BH",
+           qvalueCutoff = 0.2,
+           pvalueCutoff = 0.05)
+gg_test
+barplot(gg_test, showCategory = length(gg_test$qvalue) )
+
 ############# enrich r ############
 # KEGG over-representaion test
 # hsa	Homo sapiens (human)
@@ -41,6 +116,8 @@ kegg_enrichr <- function(genelist){
   return( kegg )
 }
 
+
+
 # dbs <- c("GO_Molecular_Function_2018", "GO_Cellular_Component_2018", "GO_Biological_Process_2018", "KEGG_2019_Human")
 GOCC_enrichr <- function(genelist){
   #########3 CpGtable a string and database a vector ############## 
@@ -54,6 +131,8 @@ GOCC_enrichr <- function(genelist){
   return( kegg )
 }
 
+# kegg_enrichr(genes)
+# GOCC_enrichr(genes)
 
 ##############33 test dataset ###################
 
@@ -76,20 +155,6 @@ GOCC_enrichr <- function(genelist){
 # GOCC_enrichr(features[modules[[1]]])
 
 
-######## find most connected hubs #############
-# transform to dataframe
-
-top_hubs_bymean <- function(abar, modules_i, n){
-  # get modules[[i]] from abar
-  df = abar[modules_i, modules_i ] %>% as.matrix() %>% as.data.frame()
-  # edges strength by mean 
-  mean_edge = mean(apply(df, 2, mean))
-  edges_nodes =  apply(df > mean_edge, 1, sum)  %>%
-    as.data.frame() %>% tibble::rownames_to_column("Nodes")
-  colnames(edges_nodes)[2] = "n_strong_edges"
-  final = edges_nodes %>% dplyr::top_n(n) %>% plyr::arrange(plyr::desc(n_strong_edges) )
-  return(final)
-}
 
 # top_hubs_bymean(abar, modules[[1]], 10)
 
