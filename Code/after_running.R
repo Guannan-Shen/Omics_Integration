727de5e199f1329f8464636d6d7e8ca31d2eb172
Guannan-Shen <guannan.shen.ai@gmail.com>
2019-10-22 23:12:02 -0600

    refine contour plot after running

---
+++ Code/after_running.R
@@ -0,0 +1,14 @@
+# After Run SmCCNet 
+
+# Optimize Contour plot 
+library(plotly)
+library(reshape2)
+library(openxlsx)
+library(tidyverse)
+library(magrittr)
+
+options(stringsAsFactors = F)
+options(dplyr.width = Inf)
+setwd("~/Documents/gitlab/Omics_Integration/DataProcessed/")
+top <- "~/Documents/gitlab/Omics_Integration/DataProcessed/"
+getwd()
